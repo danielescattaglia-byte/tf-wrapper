bc82a148a50403f31c2a8fd73bf8e2301414b1eb
danielescattaglia-byte <daniele.scattaglia@dinova.one>
2026-04-13 11:56:15 +0200

    Update file path format for generated output

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ module "file_generator" {
   source = "git::https://github.com/danielescattaglia-byte/tf-module.git?ref=1.0.0"
 
   # Costruisce il percorso e il contenuto passandoli al modulo finale
-  file_path = "${path.root}/generated_${var.app_name}.txt"
+  file_path = "${path.root}/generated_${var.app_name}_v2.txt"
   content   = "Log Generato per: ${var.app_name}\nMessaggio: ${var.message}"
 }
