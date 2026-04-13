5c6b87c1bac7ad919b4eb8c9cd56cd31db40c9cd
danielescattaglia-byte <daniele.scattaglia@dinova.one>
2026-04-13 11:52:26 +0200

    Update Git reference in main.tf module source

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "file_generator" {
   # Richiama il modulo finale via Git
-  source = "git::https://github.com/danielescattaglia-byte/tf-module.git?ref=v1.0.0"
+  source = "git::https://github.com/danielescattaglia-byte/tf-module.git?ref=1.0.0"
 
   # Costruisce il percorso e il contenuto passandoli al modulo finale
   file_path = "${path.root}/generated_${var.app_name}.txt"
